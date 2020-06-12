122ec0c05bc1b4b50505bc80330a97fa0b86d6f2
kalbert83 <mat17031@byui.edu>
2020-06-12 00:13:07 -0600

    "update"

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './home.css';
-import { url } from 'inspector';
 
 const home = {
-
-    imageSrc: url('./pictureFrame1.jpg'),
+    imageSrc: "./",
+    //imageSrc:  './pictureFrame1.jpg',
     name: 'Kalbert Mata',
     career: 'Web Developer'
 }
@@ -14,7 +13,8 @@ class Home extends React.Component {
         return (
         <div className="Business">
             <div className="image-container">
-              <img src = "./pictureFrame.jpg" alt="Kalbert"/>
+              <img src = "./pictureFrame.jpg" alt='k'/>
+              <img src = {home.imageSrc} />
             </div>
             <h1>{home.name}</h1>
             <div className="Business-information">
